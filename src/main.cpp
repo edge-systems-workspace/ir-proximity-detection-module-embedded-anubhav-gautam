71284193a4ef822e53b871395c1daa8c61131255
Anubhav Gautam <anubhavgautam9720169985@gmail.com>
2026-02-20 09:35:22 +0530

    INtializing the value of ir pin

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,15 @@
 /**
  * @file main.cpp
  * @brief Embedded Obstacle Detection System using IR Sensor
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @authorANUBHAV GAUTAM
+ * @date 2026-01-7
  *
  * @details
  * Reads digital input from IR sensor
  * and detects obstacle presence.
  */
 
- // TODO 1:
- // Define IR sensor digital pin (Use pin 2)
+int irPin = 2;
 
  // TODO 2:
  // Create variable to store sensor state
